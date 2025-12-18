ae27bd10142937430bb6418814ab75e2c10032a3
NicolaiDG <nicolaidegroot1@gmail.com>
2025-12-18 17:01:34 +0100

    Venue Api functionality added on frontend

--- CA2_Nicolai_de_Groot/Pages/Venues.cshtml.cs
+++ CA2_Nicolai_de_Groot/Pages/Venues.cshtml.cs
@@ -1,12 +1,31 @@
-using Microsoft.AspNetCore.Mvc;
+using CA2_Nicolai_de_Groot.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net.Http.Json;
 
 namespace CA2_Nicolai_de_Groot.Pages
 {
     public class VenuesModel : PageModel
     {
-        public void OnGet()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public VenuesModel(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public List<Venue> Venues { get; set; } = new();
+
+        public async Task OnGetAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var result = await client.GetFromJsonAsync<List<Venue>>(
+                "https://localhost:7119/api/Venues");
+
+            if (result != null)
+            {
+                Venues = result;
+            }
         }
     }
 }
